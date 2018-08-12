acac720e5f86ccc3eaf38c381647a9e758edcee5
Abdulrahman elzahaby <el3zahaby@gmail.com>
2018-08-12 22:42:54 +0300

    Update elza.slide.js

--- elza.slide.js
+++ elza.slide.js
@@ -1,3 +1,5 @@
+// SElector > item item item
+
         var current_img = 1;
         var i;
         (function($) {
@@ -7,16 +9,42 @@
 //                default options.
                 var settings = $.extend({
                     // These are the defaults.
-                    in: "slow",
-                    prev: '<button type="button" class="prev-elzaSlide" class="btn">Previous</button>',
-                    next: '<button type="button" class="next-elzaSlide" class="btn">Next</button>',
-                }, options);
+                    time: 1000,
+                    prev: '<button type="button" class="elzaSlide-prev btn">Previous</button>',
+                    next: '<button type="button" class="elzaSlide-next btn">Next</button>',
+                    animateUrl: 'https://cdn.rawgit.com/daneden/animate.css/master/animate.min.css',
+                    animateClassIn:'bounceInLeft',
+                    animateClassOut:'bounceInRight',
+                    auto:[false,2000],
+                    itemsCss: {
+                        'property': 'value',
+                        'property': 'value'
+                    },
 
+                }, options);
                 var imgs = this.find("div.item");
                 imgs.slice(1).hide();
-                console.log(imgs.length);
-                console.log( $(imgs).data("img"));
-                
+
+
+                // IFs
+                if (settings.animateUrl != "" || settings.animateUrl != false){
+                    $('head').append('<link rel="stylesheet" href="'+ settings.animateUrl +'" type="text/css" />');
+                }
+                if (settings.itemsCss != "" || settings.itemsCss != false){
+                    imgs.css(settings.itemsCss);
+                }
+                if (settings.auto[0] == true){
+                 setInterval(function () {
+                     if (current_img == imgs.length) {
+                         current_img = 1;
+                     } else {
+                         current_img = current_img + 1
+                     }
+                     show_img();
+                 },settings.auto[1]);
+                }
+
+
 
                 
                 $( this ).append( settings.prev );
@@ -27,14 +55,14 @@
                 function show_img() {
                     for (i = 1; i < imgs.length + 1; i++) {
                         if (i == current_img) {
-                            $(imgs[i - 1]).fadeIn(settings.in);
+                            $(imgs[i - 1]).addClass("animated " + settings.animateClassIn).removeClass(settings.animateClassOut).show();
                         } else {
-                            $(imgs[i - 1]).hide()
+                            $(imgs[i - 1]).addClass("animated " + settings.animateClassOut).removeClass(settings.animateClassIn).hide();
                         }
                     }
                 }
 
-                $(".prev-elzaSlide").click(function() {
+                $(".elzaSlide-prev").click(function() {
                     if (current_img == 1) {
                         current_img = imgs.length;
                     } else {
@@ -44,7 +72,7 @@
 
                 });
 
-                $(".next-elzaSlide").click(function() {
+                $(".elzaSlide-next").click(function() {
                     if (current_img == imgs.length) {
                         current_img = 1;
                     } else {
